67fa2d070a997e8e8d033a2fbb8793b49550346a
KarenVT <karenkvot@gmail.com>
2025-04-22 17:45:22 -0500

    Cambiar a HashRouter para solucionar problema de rutas en GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import "./styles/global.css"
 import { CartProvider } from './context/CartContext'
@@ -8,12 +8,12 @@ import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
